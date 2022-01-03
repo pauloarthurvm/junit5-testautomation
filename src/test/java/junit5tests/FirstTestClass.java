3fb87fcdb609dc749b18766137f781f32fd21805
Paulo Varassin <pmoreira@example.com>
2022-01-03 11:31:00 -0300

    Lifecycle methods

--- src/test/java/junit5tests/FirstTestClass.java
+++ src/test/java/junit5tests/FirstTestClass.java
@@ -1,9 +1,33 @@
 package junit5tests;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
+
+//@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+//This is to dont have to create static non-test methods
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class FirstTestClass {
+
+    @BeforeAll
+    void beforeAll() {
+        System.out.println("-- This is beforeAll() method.");
+    }
+
+    @BeforeEach
+    void beforeEah() {
+        System.out.println("---- This is beforeEach() method.");
+    }
+
+    @AfterAll
+    void afterAll() {
+        System.out.println("-- This is afterAll() method.");
+    }
+
+    @AfterEach
+    void afterEach() {
+        System.out.println("---- This is afterEach() method.");
+    }
 
 //    accessModifier returnType nameOfMethod(params)
     @Test
